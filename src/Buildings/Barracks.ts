bde4e0980c231b2d671e44c8e2d70005b53225af
Aozin <59066161+Aozin@users.noreply.github.com>
2021-05-16 16:34:10 +0100

    name generation

--- src/Buildings/Barracks.ts
+++ src/Buildings/Barracks.ts
@@ -10,7 +10,7 @@ export class Barracks extends Building {
     name: string;
 
     private productionPriorities: number[];
-	private productionQueue: {[priority: number]: SpawnOrder[]}; // Prioritized spawning queue
+	private productionQueue: {[priority: number]: SpawnOrder[]}; // Prioritized queue of spawning orders
 
     constructor(capital: Capital, mainSpawn: StructureSpawn) {
         super(capital, mainSpawn)
@@ -27,8 +27,27 @@ export class Barracks extends Building {
         this.manager = new QueenManager(this)
     }
 
+    //TODO MAKE A PROPER IDLE SPOT. HIGH PRIO
     get idlePos(): RoomPosition {
         return this.spawns[0].pos.getAdjacentPositions()[0]
     }
 
+    private generateCreepName(roleName: string): string {
+        // Generate a creep name based on the role and add a hex id to make it unique
+		let i = generateCreepID();
+		while (Game.creeps[(roleName + '_' + i)]) {
+			i = generateCreepID();
+		}
+		return (roleName + '_' + i);
+    };
+
+}
+
+function generateCreepID() {
+    const hex = '0123456789ABCDEF';
+    let creepID = '';
+    for (let i = 0; i < 4; i++) {
+        creepID += hex.charAt(Math.floor(Math.random() * hex.length));
+    }
+    return creepID
 }
