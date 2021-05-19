629184ec5f16f3561492196b8025aa8c83227041
Aozin <59066161+Aozin@users.noreply.github.com>
2021-05-19 13:20:11 +0100

    Added all creep actions (maybe)

--- src/Creep/Creep_Actions.ts
+++ src/Creep/Creep_Actions.ts
@@ -1,6 +1,11 @@
 
 interface Creep {
+    goBuild(target: ConstructionSite): void;
+    goRepair(target: Structure): void;
+    goTransfer(target: Creep | Structure, resource?: ResourceConstant): void;
+    goWithdraw(target: Structure | Tombstone, resource?: ResourceConstant): void;
     goHarvest(target: Source | Mineral | Deposit): void;
+    goDrop(target: RoomPosition, resource?: ResourceConstant): void;
 }
 
 
@@ -14,9 +19,49 @@ const RANGES = {
 };
 
 
+
+Creep.prototype.goBuild = function(target: ConstructionSite) {
+    if (this.pos.inRangeTo(target.pos, RANGES.BUILD)) {
+        this.build(target)
+    } else {
+        this.travelTo(target)
+    }
+}
+
+Creep.prototype.goRepair = function(target: Structure) {
+    if (this.pos.inRangeTo(target.pos, RANGES.REPAIR)) {
+        this.repair(target)
+    } else {
+        this.travelTo(target)
+    }
+}
+Creep.prototype.goTransfer = function(target: Creep | Structure, resource: ResourceConstant = RESOURCE_ENERGY) {
+    if (this.pos.inRangeTo(target.pos, RANGES.TRANSFER)) {
+        this.transfer(target, resource)
+    } else {
+        this.travelTo(target)
+    }
+}
+
+Creep.prototype.goWithdraw = function(target: Structure | Tombstone, resource: ResourceConstant = RESOURCE_ENERGY) {
+    if (this.pos.inRangeTo(target.pos, RANGES.TRANSFER)) {
+        this.withdraw(target, resource)
+    } else {
+        this.travelTo(target)
+    }
+}
+
 Creep.prototype.goHarvest = function(target: Source | Mineral | Deposit) {
     if (this.pos.inRangeTo(target.pos, RANGES.HARVEST)) {
         this.harvest(target)
+    } else {
+        this.travelTo(target)
+    }
+}
+
+Creep.prototype.goDrop = function(target: RoomPosition, resource: ResourceConstant = RESOURCE_ENERGY) {
+    if (this.pos.inRangeTo(target, RANGES.DROP)) {
+        this.drop(resource)
     } else {
         this.travelTo(target)
     }
