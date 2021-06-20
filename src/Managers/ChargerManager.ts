ea60b3eb5cff9527547e6e90f51678182300f01c
Aozin <59066161+Aozin@users.noreply.github.com>
2021-06-20 23:56:01 +0100

    Finished off hardcoded positions
    Hopefully I didn't mess anything up

--- src/Managers/ChargerManager.ts
+++ src/Managers/ChargerManager.ts
@@ -1,24 +1,27 @@
 import { CreepSetup } from "Creep_Setups/CreepSetup";
 import { Roles, Setups } from "Creep_Setups/Setups";
 import { Manager } from "Manager";
-import { bunkerPath } from "Room/Bunker";
+import { Bunker} from "Room/Bunker";
 import { Capital } from "Room/Capital";
 import { ManagerPriority } from "./ManagerPriority";
 
 export abstract class ChargerManager extends Manager {
     chargers: Creep[]
     setup: CreepSetup
-    offset: {x: number, y: number}
     idlePoint: RoomPosition
-    fillTargets: (StructureExtension | StructureTower)[]
+    anchor: RoomPosition
+    fillTargets: (StructureExtension | StructureTower | StructureSpawn)[]
     path: RoomPosition[]; //The clockwise path the filler will take
-    constructor(capital: Capital, prio = ManagerPriority.Core.charger, direction: DirectionConstant) {
-        super({pos:capital.anchor.findPositionAtDirection(direction), capital: capital}, "ChargerManager_" + capital.name + "_" + direction, prio)
+    constructor(capital: Capital, prio = ManagerPriority.Core.charger, direction: 6 | 8 | 2) {
+        let idle = capital.anchor!.findPositionAtDirection(direction)
+        super({pos: idle, capital: capital}, "ChargerManager_" + capital.name + "_" + direction, prio)
+        this.idlePoint = idle
         this.setup = Setups.chargers.default
-        let anchor = this.capital.anchor
+        this.anchor = this.capital.anchor!
         this.chargers = this.creepsByRole[Roles.charger]
-        this.path = bunkerPath[direction]
-        this.fillTargets =
+        this.path = Bunker.getPathFromList(this.anchor, Bunker.bunkerPath[direction])
+
+        this.fillTargets = Bunker.getFillStructuresFromList(this.anchor, Bunker.bunkerFillTargets[direction])
 
     }
 
